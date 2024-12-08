df29bdc24661d42342441764a510b9ae62ade198
Mateus Acauhi <mateusacauhi@bluems.com>
2024-12-08 19:17:45 -0300

    add: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import type { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("RealTime Chat")
+    .setDescription("My first study about websockets")
+    .setVersion("1.0.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(process.env.SERVER_PORT || 3000);
 }
 bootstrap();
