d2273f20b6d4869c59bc565458c29c2831191a94
Mateus Acauhi <mateusacauhi@bluems.com>
2024-12-08 19:57:47 -0300

    fix: ajuste no path do database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { UserModule } from "./user/user.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import { getDatabaseConfig } from "./database.config";
+import { getDatabaseConfig } from "./config/database.config";
 
 @Module({
   imports: [
