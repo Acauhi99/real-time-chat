c290d2b4466a45f7002e6ed1f257148029fd4d79
Mateus Acauhi <mateusacauhi@bluems.com>
2024-12-22 12:34:11 -0300

    refactor: melhora nos padroes de resposta

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,37 +6,47 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  HttpStatus,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
+import { DataUserResponseDto } from "./dto/data-user-response.dto";
 
 @Controller("user")
+@UseInterceptors(ClassSerializerInterceptor)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto): Promise<DataUserResponseDto> {
     return this.userService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<DataUserResponseDto[]> {
     return this.userService.findAll();
   }
 
   @Get(":id")
-  findOne(@Param("id") id: string) {
+  findOne(@Param("id") id: string): Promise<DataUserResponseDto | null> {
     return this.userService.findOne(id);
   }
 
   @Patch(":id")
-  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(
+    @Param("id") id: string,
+    @Body() updateUserDto: UpdateUserDto
+  ): Promise<DataUserResponseDto> {
     return this.userService.update(id, updateUserDto);
   }
 
   @Delete(":id")
-  remove(@Param("id") id: string) {
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param("id") id: string): Promise<void> {
     return this.userService.remove(id);
   }
 }
