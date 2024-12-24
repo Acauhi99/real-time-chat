93152df43648d89380912e850752143eb1127af8
Mateus Acauhi <mateusacauhi@bluems.com>
2024-12-24 00:32:28 -0300

    add: interceptor e filter http

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,10 @@ import { UserModule } from "./user/user.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { getDatabaseConfig } from "./config/database.config";
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
+import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
+import { LoggingInterceptor } from "./interceptor";
+import { HttpExceptionFilter } from "./exception/http.exception.filter";
 
 @Module({
   imports: [
@@ -18,6 +21,16 @@ import { AuthModule } from './auth/auth.module';
     }),
     UserModule,
     AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
   ],
 })
 export class AppModule {}
