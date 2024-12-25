0b28c91467b420bcb6e3f471ef3f7fff12ea1fbb
Mateus Acauhi <mateusacauhi@bluems.com>
2024-12-25 11:17:25 -0300

    refactor: configs para modulo de autenticacao

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserRepository } from "src/user/user.repository";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { UserModule } from "src/user/user.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { getJwtConfig } from "../config/jwt.config";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    UserModule,
+    PassportModule.register({ defaultStrategy: "jwt" }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => getJwtConfig(configService),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
