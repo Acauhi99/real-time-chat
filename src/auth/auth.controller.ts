6d3f70c12f853deb39bbf18a5f69fb9a640f4d8a
Mateus Acauhi <mateusacauhi@bluems.com>
2024-12-25 11:16:55 -0300

    add: endpoints de registro e login de usuario

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { CreateUserDto } from "../user/dto/create-user.dto";
+import { AuthService } from "./auth.service";
+import { Body, Controller, Post } from "@nestjs/common";
+import { SignInDto } from "./dto/sign-in.dto";
 
-@Controller('auth')
-export class AuthController {}
+@Controller("auth")
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post("signup")
+  async signUp(@Body() createUserDto: CreateUserDto): Promise<void> {
+    await this.authService.signUp(createUserDto);
+  }
+
+  @Post("signin")
+  async signIn(@Body() signInDto: SignInDto): Promise<{ accessToken: string }> {
+    return await this.authService.signIn(signInDto);
+  }
+}
